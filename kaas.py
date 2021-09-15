2f33301186f4ff8b755a016a905575314f79325f
Riad Hak <99054420@mydavinci.nl>
2021-09-15 11:35:48 +0200

    kaas.py

---
+++ kaas.py
@@ -0,0 +1,21 @@
+kaas = input('is de kaas geel? ')
+
+if kaas =='ja':
+    vraag1 = input('zit er gaten in? ')
+    if vraag1 =='ja':
+          vraag2 = input('is de kaas bruin? ')
+          if vraag2 =='ja': 
+              print('dan is het een alberthein kaas. ')
+    else: 
+        vraag3 = input('zit er groene plekken op de kaas? ')
+    
+    if vraag3 == 'ja': print('dan is het een franse kaas ')
+
+else: 
+    vraag4= input('is de kaas wit? ') 
+    if vraag4 == 'ja':      
+        print('dan is het een lidl kaas ')
+    else : 
+        vraag5 = input('heeft de kaas een blaauwe verpaking? ')
+    if vraag5 == 'ja':
+        print('dan is het een jumbo kaas. ')
