5495044ab7fef92fde9b1aef808051edccbcea4f
Riad Hak <99054420@mydavinci.nl>
2021-09-20 14:23:06 +0200

    game.py

--- soli.py
+++ soli.py
@@ -12,7 +12,6 @@ certifi  = input('heb je een certificaat van overleven met gevaarlijke personeel
 
 
 
-
 if evrvaring >=4 and (diploma == "mbo4" or diploma == "hbo" or diploma == 'uni' or diploma == 'universiteit') and hoed == "ja" and (snor >=10 or haar=='ja') and lengte >=150 and gewicht >=90 and certifi =="ja":
     print('jij voeldoet aan de eisen, we nemen zo snel mogelijk contact met je op. ')
 else :
