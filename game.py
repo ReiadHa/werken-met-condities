4181e921abe6e003fd0b46ce76e49bcbcdd35122
ReiadHa <89877825+ReiadHa@users.noreply.github.com>
2021-09-22 12:51:13 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,90 @@
-print('You are now in a dungeon and depends on your options u might just barly survive')
-print('=============================================================================')
+boss = """
+              __.......__
+            .-:::::::::::::-.
+          .:::''':::::::''':::.
+        .:::'     `:::'     `:::. 
+   .'\  ::'   ^^^  `:'  ^^^   '::  /`.
+  :   \ ::   _.__       __._   :: /   ;
+ :     \`: .' ___\     /___ `. :'/     ; 
+:       /\   (_|_)\   /(_|_)   /\       ;
+:      / .\   __.' ) ( `.__   /. \      ;
+:      \ (        {   }        ) /      ; 
+ :      `-(     .  ^"^  .     )-'      ;
+  `.       \  .'<`-._.-'>'.  /       .'
+    `.      \    \;`.';/    /      .'
+      `._    `-._       _.-'    _.'
+       .'`-.__ .'`-._.-'`. __.-'`.
+     .'       `.         .'       `.
+   .'           `-.   .-'           `.
+   
+
+"""
+finger = """
+....................../´¯/) 
+....................,/¯../ 
+.................../..../ 
+............./´¯/'...'/´¯¯`·¸ 
+........../'/.../..../......./¨¯\ 
+........('(...´...´.... ¯~/'...') 
+.........\.................'...../ 
+..........''...\.......... _.·´ 
+............\..............( 
+..............\.............\...
+"""
+sad = """
+     .-""""""-.
+   .'          '.
+  /   O      O   } 
+ :           `    :
+ |                |   
+ :    .------.    :
+  \  '        '  /
+   '.          .'
+     '-......-'
+"""
+
+haha = '''
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'               `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$'                   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$$'`$$$$$$$$$$$$$'`$$$$$$!                       !$$$$$$'`$$$$$$$$$$$$$'`$$$
+$$$$  $$$$$$$$$$$  $$$$$$$                         $$$$$$$  $$$$$$$$$$$  $$$$
+$$$$. `$' \ \$`  $$$$$$$!                         !$$$$$$$  '$/ `/ `$' .$$$$
+$$$$$. !\  i  i .$$$$$$$$                           $$$$$$$$. i  i  /! .$$$$$
+$$$$$$   `--`--.$$$$$$$$$                           $$$$$$$$$.--'--'   $$$$$$
+$$$$$$L        `$$$$$^^$$                           $$^^$$$$$'        J$$$$$$
+$$$$$$$.   .'   ""~   $$$    $.                 .$  $$$   ~""   `.   .$$$$$$$
+$$$$$$$$.  ;      .e$$$$$!    $$.             .$$  !$$$$$e,      ;  .$$$$$$$$
+$$$$$$$$$   `.$$$$$$$$$$$$     $$$.         .$$$   $$$$$$$$$$$$.'   $$$$$$$$$
+$$$$$$$$    .$$$$$$$$$$$$$!     $$`$$$$$$$$'$$    !$$$$$$$$$$$$$.    $$$$$$$$
+$JT&yd$     $$$$$$$$$$$$$$$$.    $    $$    $   .$$$$$$$$$$$$$$$$     $by&TL$
+                                 $    $$    $
+                                 $.   $$   .$
+                                 `$        $'
+                                  `$$$$$$$$'
+'''
+like = '''
+░░░░░░░░░░░░░░░░░░░░░░█████████░░░░░░░░░
+░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███░░░░░░░
+░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███░░░░
+░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░
+░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███░
+░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░
+░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░
+░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█░░░░░
+░░████████████░░░█████████████████░░░░░░ 
+'''
 
 game = input('Type yes to start the game: ')
+print('You are now in a dungeon and depends on your options u might just barly survive')
+print('=============================================================================')
 print('')
-print('a groupe of monsters are heading toward you and u have a few seconds to decide what to do, choose wisly! ')
+print('a group of monsters are heading toward you and u have a few seconds to decide what to do, choose wisly! ')
 print('')
 print('there is a machien gun on your left rear, and a door that could be locked on ur right! ')
 print('there also a mysterious treasure room behind u! ')
@@ -12,6 +93,7 @@ print('do u want to pick up the gun and kill the monsters, or run away through t
 print('there is also a thrid option wich is for')
 machiengun = input(' type "gun" for gun and "door" for the door and "room" to go the mysteirous treasure room. ')
 if machiengun == 'gun' :
+    print('')
     print('u have killed the monsters and pissed off their leader and now he is coming for u! ')
     print('')
     print('now that their leader is coming for u u have tow choices: ')
@@ -19,18 +101,16 @@ if machiengun == 'gun' :
     print('2- reload your machiengun and fight their leader ')
     leader = input('Type "1" or "2" fill in ur answer befor their leader rip u apart :D ')
     if leader == '1' :
+        print('')
         print('you ran from their leader and hid behind the rock ')
         print('their leader found cuz u were afriad to attack and killed you ')
         print('monsters have won... ')
         print('You have Lost:) ')
-        print('                 _.------.                        .----.__')
-        print('           /         \_.       ._           /---.__  ')
-        print('          |  O    O   |\\___  //|          /       `\ |' )
-        print('          |  .vvvvv.  | )   `(/ |         | o     o  \|' )
-        print('          /  |     |  |/      \ |  /|   ./| .vvvvv.  |' )
+        print(str(boss))
 
 
-    else :  
+    else :
+        print('')
         print('it was an intense fight and ur are bleeding, but u sucsessfully killed their leader and left the dungeon! ')
         print('congratulations u have WON! ')
         print('..... (¯`v´¯)♥')
@@ -42,12 +122,14 @@ if machiengun == 'gun' :
         print('/ \ ♥♥')
  
 if machiengun =='door' :
+    print('')
     print('u ran away from the monsters, but now u cant go back from the same door cuz its locked! ')
     print('u have to fine another way out of this dungeon! ') 
     print('u have a split way ahead of you! it could lead to your death choose wisly ')
     door = input('Type in "right" to go right and "left" to go left: ')
    
     if door == 'right' :
+        print('')
         print('after u went right there is might be a trap so be carfull. ')
         print('')
         print('u have 2 options: ')
@@ -56,11 +138,15 @@ if machiengun =='door' :
         print('')
         trap = input('Type "1" to run through, type "2" to walk carfully. ')
         if trap == '1' :
+            print('')
             print('There wasnt any Traps Hooray, or so u thought. ')
             print('there was indeed no traps but there were monster sleeping around and heard you running and came for you and killed u. ')
             print('')
-            print('You Lost :)')
+            print('YOU LOST :)')
+            print(str(haha))
+
     else : 
+        print('')
         print('now that u have chosen ur path u have to make one more decision!. ')
         print('')
         print('u have 2 options: ')
@@ -70,11 +156,14 @@ if machiengun =='door' :
         trap = input('Type "1" to run through, type "2" to walk carfully. ')
         print('')
         if trap == '1' :
+             print('')
              print('There wasnt any Traps Hooray, or so u thought. ')
              print('there was indeed no traps but there were monster sleeping around and heard you running and came for you and killed u. ')
              print('')            
-             print('You Lost :)')
+             print('YOU LOST  :)')
+             print(str(sad))
         else :
+          print('')
           print('u walked carfully even when u knew that u might not be fast enough to react to the trap ')
           print('never the less that was the right choice. ')
           print('because there were mosnters sleeping in the shadows and in u didnt wake them up. ')
@@ -89,7 +178,8 @@ if machiengun =='door' :
           print('/▌♥♥')
           print('/ \ ♥♥')
 
-if machiengun == "room": 
+if machiengun == "room":
+    print('')
     print('u did get in the treasure room but the leader is still persuing u like there is no tomorrow! ')
     print('in the mysterious treasure room, and u find a weird fruit! ')
     print('and under the mysterious fruit there is an explanation written in ancient langauge , and it says: ')
@@ -98,10 +188,11 @@ if machiengun == "room":
     print('u can take 1 bite from it, take 2 bites or take 3 bites one can give u powers and the rest can kill you! choose wisly ')
     fruit = int(input('How many bites do u want to take? '))
     if fruit >= 3 :
-        print('u died')
-    if fruit >= 2 : 
-        print('u died')
-    if fruit <= 1 : 
+        print('U DIED:) :) :)' + str(finger))
+    if fruit == 2 : 
+        print('U DIED!!!!!!!' + str(boss))
+    if fruit == 1 : 
         print('u gained the power, and killed the mosnter king!')
         print('')
-        print('u have won, congratulations:D')
+        print('u have won, congratulations:D' + str(like))
+
